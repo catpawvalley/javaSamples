692dd1e8bd92a9fe23a4279229c9fcc48b6b5347
catpawvalley <148180090+catpawvalley@users.noreply.github.com>
2023-11-18 19:43:02 -0800

    Create blankFrame.java

---
+++ blankUI/blankFrame.java
@@ -0,0 +1,42 @@
+package blankUI;
+import javax.swing.JFrame;
+
+public class blankFrame extends JFrame implements Runnable{
+
+	public static final int basicSq = 400;
+
+	public blankFrame(){
+		super();
+		setSize(basicSq, basicSq);
+	}
+
+	public blankFrame(String n){
+		super(n);
+		setSize(basicSq, basicSq);
+	}
+
+	public blankFrame(int w, int h){
+		super();
+		setSize(w, h);
+	}
+
+	public blankFrame(String n, int w, int h){
+		super(n);
+		setSize(w, h);
+	}
+
+	public blankFrame(int sq){
+		super();
+		setSize(sq, sq);
+	}
+
+	public blankFrame(String n, int sq){
+		super(n);
+		setSize(sq, sq);
+	}
+
+	public void run(){
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+}
